f3dce4225c9f5a6922011074104b29ee039e7840
Tahar <tntmeijs@gmail.com>
2020-12-01 19:18:58 +0100

    First solution for day one

--- day_one/src/lib.rs
+++ day_one/src/lib.rs
@@ -19,7 +19,22 @@ impl shared::puzzle_input::PuzzleTrait for DayOne {
         self.input = shared::puzzle_input::read_input_as_vec("D:/Projects/aoc_2020/input/day_one.txt");
     }
 
-    fn solve_part_one(&self) {}
+    // Part one: in the input list, find two entries that sum to 2020 and multiply them
+    fn solve_part_one(&self) {
+        // Need a vector of integers
+        let vec_ints: Vec<u32> = self.input.iter().map(|value| value.parse().unwrap()).collect();
+
+        // I <3 brute force solutions!
+        for value_a in &vec_ints {
+            for value_b in &vec_ints {
+                if value_a + value_b == 2020 {
+                    println!("{} + {} = 2020", value_a, value_b);
+                    println!("Answer part one: {}", value_a * value_b);
+                    return
+                }
+            }
+        }
+    }
 
     fn solve_part_two(&self) {}
 }
