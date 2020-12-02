0fd7873931dcbaadd0ef3def38694c30bfcc33b7
Tahar <tntmeijs@gmail.com>
2020-12-02 13:25:25 +0100

    First solution day 2

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -1,11 +1,19 @@
 use day_one::solution::logic::DayOne;
+use day_two::solution::logic::DayTwo;
 use shared::traits::puzzle_trait::PuzzleTrait;
 
 fn main() {
     println!("Learning Rust with the Advent of Code!");
     let mut day_1 = DayOne::new();
+    let mut day_2 = DayTwo::new();
+
     day_1.print_info();
     day_1.gather_input();
     day_1.solve_part_one();
     day_1.solve_part_two();
+
+    day_2.print_info();
+    day_2.gather_input();
+    day_2.solve_part_one();
+    day_2.solve_part_two();
 }
--- day_two/src/solution/logic.rs
+++ day_two/src/solution/logic.rs
@@ -17,11 +17,55 @@ impl PuzzleTrait for DayTwo {
     }
 
     fn gather_input(&mut self) {
-        self.input = read_input_as_vec("./input/day_one.txt");
+        self.input = read_input_as_vec("./input/day_two.txt");
     }
 
     // Part one: find how many passwords are valid according to the rules
     fn solve_part_one(&self) {
+        let mut valid_password_count: u32 = 0;
+
+        for input in &self.input {
+            let parts: Vec<&str> = input.split(':').collect();
+            
+            // Invalid input
+            if parts.len() != 2 {
+                continue;
+            }
+
+            // Relevant input data without whitespaces
+            let policy = parts[0].trim();
+            let password = parts[1].trim();
+
+            // Deconstruct the policy string into the information and the character it applies to
+            let policy_parts: Vec<&str> = policy.split(' ').collect();
+
+            // Invalid input
+            if policy_parts.len() != 2 {
+                continue;
+            }
+
+            let policy_info = policy_parts[0];
+            let target_character = policy_parts[1];
+
+            // Deconstruct policy info
+            let min_max: Vec<usize> = policy_info.split('-').map(|value| value.parse().unwrap()).collect();
+
+            // Invalid input
+            if min_max.len() != 2 {
+                continue;
+            }
+
+            let min = min_max[0];
+            let max = min_max[1];
+
+            // Password validation
+            let occurances = password.matches(target_character).count();
+            if occurances >= min && occurances <= max {
+                valid_password_count += 1;
+            }
+        }
+
+        println!("Answer part one: {}", valid_password_count);
     }
 
     // Part two: ___
