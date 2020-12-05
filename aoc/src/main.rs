2d7e57f1321ef9873c783f7696aebe219725acb9
Tahar <tntmeijs@gmail.com>
2020-12-05 09:25:45 +0100

    Execute day 5 functions

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -2,38 +2,47 @@ use day_01::solution::Day01;
 use day_02::solution::Day02;
 use day_03::solution::Day03;
 use day_04::solution::Day04;
+use day_05::solution::Day05;
 use shared::puzzle_trait::PuzzleTrait;
 
 // #TODO: Since these objects all implement PuzzleTrait we can put them in a Vec<PuzzleTrait>
 fn main() {
-    let mut day_1 = Day01::new();
-    let mut day_2 = Day02::new();
-    let mut day_3 = Day03::new();
-    let mut day_4 = Day04::new();
+    let mut day_01 = Day01::new();
+    let mut day_02 = Day02::new();
+    let mut day_03 = Day03::new();
+    let mut day_04 = Day04::new();
+    let mut day_05 = Day05::new();
 
-    day_1.print_info();
-    day_1.gather_input();
-    day_1.solve_part_one();
-    day_1.solve_part_two();
-
-    println!();
-
-    day_2.print_info();
-    day_2.gather_input();
-    day_2.solve_part_one();
-    day_2.solve_part_two();
+    day_01.print_info();
+    day_01.gather_input();
+    day_01.solve_part_one();
+    day_01.solve_part_two();
 
     println!();
 
-    day_3.print_info();
-    day_3.gather_input();
-    day_3.solve_part_one();
-    day_3.solve_part_two();
+    day_02.print_info();
+    day_02.gather_input();
+    day_02.solve_part_one();
+    day_02.solve_part_two();
 
     println!();
 
-    day_4.print_info();
-    day_4.gather_input();
-    day_4.solve_part_one();
-    day_4.solve_part_two();
+    day_03.print_info();
+    day_03.gather_input();
+    day_03.solve_part_one();
+    day_03.solve_part_two();
+
+    println!();
+
+    day_04.print_info();
+    day_04.gather_input();
+    day_04.solve_part_one();
+    day_04.solve_part_two();
+
+    println!();
+
+    day_05.print_info();
+    day_05.gather_input();
+    day_05.solve_part_one();
+    day_05.solve_part_two();
 }
