95803f73f7483ea625f6b2229bc96fd697495c03
Tahar <tntmeijs@gmail.com>
2020-12-06 10:05:26 +0100

    Puzzles are stored in a vector to reduce code duplication

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -6,52 +6,29 @@ use day_05::solution::Day05;
 use day_06::solution::Day06;
 use shared::puzzle_trait::PuzzleTrait;
 
-// #TODO: Since these objects all implement PuzzleTrait we can put them in a Vec<PuzzleTrait>
 fn main() {
-    let mut day_01 = Day01::new();
-    let mut day_02 = Day02::new();
-    let mut day_03 = Day03::new();
-    let mut day_04 = Day04::new();
-    let mut day_05 = Day05::new();
-    let mut day_06 = Day06::new();
+    // List of pointers to the puzzle objects
+    let mut puzzles: Vec<Box<dyn PuzzleTrait>> = Vec::new();
 
-    day_01.print_info();
-    day_01.gather_input();
-    day_01.solve_part_one();
-    day_01.solve_part_two();
+    // Instantiate all puzzles
+    puzzles.push(Box::new(Day01::new()));
+    puzzles.push(Box::new(Day02::new()));
+    puzzles.push(Box::new(Day03::new()));
+    puzzles.push(Box::new(Day04::new()));
+    puzzles.push(Box::new(Day05::new()));
+    puzzles.push(Box::new(Day06::new()));
 
-    println!();
+    // Run all puzzles
+    let puzzle_count = puzzles.len();
+    for (index, puzzle) in puzzles.iter_mut().enumerate() {
+        puzzle.print_info();
+        puzzle.gather_input();
+        puzzle.solve_part_one();
+        puzzle.solve_part_two();
 
-    day_02.print_info();
-    day_02.gather_input();
-    day_02.solve_part_one();
-    day_02.solve_part_two();
-
-    println!();
-
-    day_03.print_info();
-    day_03.gather_input();
-    day_03.solve_part_one();
-    day_03.solve_part_two();
-
-    println!();
-
-    day_04.print_info();
-    day_04.gather_input();
-    day_04.solve_part_one();
-    day_04.solve_part_two();
-
-    println!();
-
-    day_05.print_info();
-    day_05.gather_input();
-    day_05.solve_part_one();
-    day_05.solve_part_two();
-    
-    println!();
-
-    day_06.print_info();
-    day_06.gather_input();
-    day_06.solve_part_one();
-    day_06.solve_part_two();
+        // Separate puzzle answers
+        if index < puzzle_count - 1 {
+            println!();
+        }
+    }
 }
