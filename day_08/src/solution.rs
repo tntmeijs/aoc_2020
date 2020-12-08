97210ad46342485e85066d50c25aa9cc4b0a43aa
Tahar <tntmeijs@gmail.com>
2020-12-08 20:01:53 +0100

    Second solution day 8

--- day_08/src/solution.rs
+++ day_08/src/solution.rs
@@ -65,8 +65,51 @@ impl PuzzleTrait for Day08 {
         }
     }
 
-    // Part two: ___
+    // Part two: change one jmp into nop or one nop into jmp, what is the value of the accumulator after the program terminates?
     fn solve_part_two(&self) {
-        println!("Answer part two: {}", -1);
+        // Oof, brute forcing the solution is not pretty and introduces O(scary) complexity :(
+        for index in 0..(self.input.len() - 1) {
+            // Early out when no nop or jmp instruction exists at this index
+            if !self.input[index].contains("jmp") && !self.input[index].contains("nop") {
+                continue;
+            }
+
+            // Change the instruction at the current index
+            // Either jmp turns into nop, or nop turns into jmp
+            let mut modified_input = self.input.clone();
+            let current_line = &modified_input[index];
+
+            if current_line.contains("jmp") {
+                modified_input[index] = current_line.replace("jmp", "nop");
+            } else if current_line.contains("nop") {
+                modified_input[index] = current_line.replace("nop", "jmp");
+            }
+
+            let mut program_counter = 0i64;
+            let mut accumulator = 0i64;
+
+            // Load instructions into "ram"
+            let mut instructions = Vec::new();
+            for line in &modified_input {
+                instructions.push(parse_line_into_instruction(line));
+            }
+
+            loop {
+                // Fetch an instruction
+                let instruction = &mut instructions[program_counter as usize];
+                instruction.execute(&mut program_counter, &mut accumulator);
+    
+                // Still an infinite loop, try to replace a different nop / jmp instruction
+                if instruction.hit_count == 2 {
+                    break;
+                }
+
+                // Program reached the end, we can now terminate
+                if program_counter as usize >= instructions.len() {
+                    println!("Answer part two: {} is the value of the accumulator after terminating", accumulator);
+                    return;
+                }
+            }
+        }
     }
 }
