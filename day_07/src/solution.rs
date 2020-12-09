95f9a6fd54c4404adeb1f836d58111e2ae2c1a80
Tahar <tntmeijs@gmail.com>
2020-12-09 20:45:31 +0100

    Attempt to use the tree for day 7

--- day_07/src/solution.rs
+++ day_07/src/solution.rs
@@ -1,47 +1,19 @@
 use shared::input::read_input_as_vec;
 use shared::puzzle_trait::PuzzleTrait;
 
+use std::collections::HashSet;
+use std::collections::BTreeMap;
+use std::collections::VecDeque;
+
 use shared::tree::Tree;
 
-// #[allow(dead_code)]
-// fn get_next_bag (input: &Vec<String>, line: usize) -> Bag {
-//     // Split bag name from child names
-//     let parts = input[line].split("contain").collect::<Vec<&str>>();
-//
-//     let bag_color = parts[0].trim().replace("bags", "").replace("bag", "");
-//     let mut child_bags = Vec::new();
-//
-//     // Find child nodes
-//     if !parts[1].trim().starts_with("no other bag") {
-//         // Get child names and counts
-//         let children = parts[1].split(", ").collect::<Vec<&str>>();
-//      
-//         for child in &children {
-//             let pretty_child = child.trim().replace(".", "").replace("bags", "").replace("bag", "");
-//
-//             // First character is the count
-//             let occurances = pretty_child.chars().nth(0).unwrap().to_digit(10).unwrap();
-//
-//             // Skip the count and a space to get the child color
-//             let child_color = pretty_child[2..].to_string();
-//
-//             for _i in 0..occurances {
-//                 child_bags.push(Bag { color: child_color.clone(), bags: Vec::new() });
-//             }
-//         }
-//     }
-//
-//     Bag { color: bag_color, bags: child_bags }
-// }
-
 pub struct Day07 {
-    input: Vec<String>,
-    current_line: usize,
+    input: Vec<String>
 }
 
 impl Day07 {
     pub fn new() -> Day07 {
-        Day07 { input: Vec::new(), current_line: 0 }
+        Day07 { input: Vec::new() }
     }
 }
 
@@ -56,54 +28,112 @@ impl PuzzleTrait for Day07 {
 
     // Part one: how many bag colors can contain at least one shiny gold bag?
     fn solve_part_one(&self) {
-        // Generate data to store in the tree below
-        let node_0_data = "root node";
-        let node_1_data = "node 1";
-        let node_2_data = "node 2";
-        let node_3_data = "node 3";
-        let node_4_data = "node 4";
-        let node_5_data = "node 5";
-        let node_6_data = "target node";
-        
-        // Construct a tree
-        let mut tree = Tree::new(node_0_data);
+        let mut tree = Tree::new(String::from("root"));
+        let root_index = tree.find_root_index();
 
-        // Add some data
-        let node_0_index = 0;
-        let node_1_index = tree.insert(node_0_index, node_1_data);
-        let node_2_index = tree.insert(node_0_index, node_2_data);
-        let node_3_index = tree.insert(node_1_index, node_3_data);
-        let node_4_index = tree.insert(node_1_index, node_4_data);
-        let node_5_index = tree.insert(node_2_index, node_5_data);
-        let node_6_index = tree.insert(node_5_index, node_6_data);
+        for line in &self.input {
+            // Split bag name from child names
+            let parts = line.split("contain").collect::<Vec<&str>>();
 
-        // Find a node with the text "target node"
-        let mut path = Vec::new();
-        let results = tree.find_all("target node");
-        if results.is_empty() {
-            println!("ERROR: Not found");
-        } else {
-            let mut node = tree.get_by_index(results[0]);
+            let bag_color = parts[0].replace("bags", "").replace("bag", "").trim().to_string();
+            let mut child_bags = Vec::new();
+
+            // Find child nodes
+            if !parts[1].trim().starts_with("no other bag") {
+                // Get child names and counts
+                let children = parts[1].split(", ").collect::<Vec<&str>>();
+            
+                for child in &children {
+                    let pretty_child = child.replace(".", "").replace("bags", "").replace("bag", "").trim().to_string();
+
+                    // First character is the count
+                    let occurances = pretty_child.chars().nth(0).unwrap().to_digit(10).unwrap();
+
+                    // Skip the count and a space to get the child color
+                    let child_color = pretty_child[2..].to_string();
+
+                    for _i in 0..occurances {
+                        child_bags.push(child_color.clone());
+                    }
+                }
+            }
+
+            // Does this bag color already exist in the tree?
+            let mut found_indices = tree.find_all(bag_color.clone());
+            if found_indices.is_empty() {
+                // Bag does not exist in the tree yet, attach it to the root node
+                found_indices.push(tree.insert(root_index, bag_color.clone()));
+            }
+
+            // Add all children to the bags in the tree
+            for index in found_indices {
+                for child in &child_bags {
+                    tree.insert(index, child.clone());
+                }
+            }
+        }
+
+        // Find all bag colors that can hold at least one shiny gold bag
+        let shiny_gold_bags = tree.find_all("shiny gold".to_string());
+        let mut unique_colors = HashSet::new();
+
+        // From the bottom of the tree, traverse upwards until the root node has been reached
+        for bag_index in shiny_gold_bags {
+            let mut current_node = tree.get_by_index(bag_index);
             loop {
-                if node.parent_index.is_none() {
+                // Reached the top of the tree
+                if current_node.parent_index.is_none() || current_node.parent_index.unwrap() == root_index {
                     break;
                 }
 
-                let parent_index = node.parent_index.unwrap();
-                path.push(parent_index);
-                node = tree.get_by_index(parent_index);
+                // Keep traversing towards the root node, saving all encountered colors in the process
+                unique_colors.insert(current_node.data.clone());
+                current_node = tree.get_by_index(current_node.parent_index.unwrap());
             }
         }
 
-        print!("Target: {} ", results[0]);
-        for (index, value) in path.iter().enumerate() {
-            print!("{}", value);
+        let mut frontier = VecDeque::new();
+        frontier.push_back(0);
 
-            if index < path.len() - 1 {
-                print!(" ");
-            }
+        let mut nodes_by_depth = BTreeMap::new();
+        for depth in 0..tree.max_depth + 1 {
+            nodes_by_depth.insert(depth, Vec::new());
         }
-        println!();
+
+        while !frontier.is_empty() {
+            // Breadth-first traversal
+            let index = frontier.pop_front().unwrap();
+            let node = tree.get_by_index(index);
+            let depth = node.depth;
+
+            // Add children to be searched next
+            for child in &node.children {
+                frontier.push_back(*child);
+            }
+
+            // Save this node
+            nodes_by_depth.get_mut(&depth).unwrap().push(node);
+        }
+
+        for (depth, nodes) in &nodes_by_depth {
+            // Print padding
+            for _i in 0..*depth {
+                print!("    ");
+            }
+
+            for (index, node) in nodes.iter().enumerate() {
+                print!("{}", node.data);
+
+                if index < nodes.len() - 1 {
+                    print!(" ");
+                }
+            }
+
+
+            println!();
+        }
+
+        println!("Answer part one: {} bags can eventually contain at least one shiny gold bag", unique_colors.len());
     }
 
     // Part two: ___
--- shared/src/tree.rs
+++ shared/src/tree.rs
@@ -1,20 +1,30 @@
 use std::collections::VecDeque;
 
+#[derive(Debug)]
 pub struct TreeNode<T: PartialEq> {
     pub data: T,
     pub index: usize,
     pub parent_index: Option<usize>,
-    pub children: Vec<usize>
+    pub children: Vec<usize>,
+    pub depth: usize,
+    is_root: bool
 }
 
+#[derive(Debug)]
 pub struct Tree<T: PartialEq> {
-    nodes: Vec<TreeNode<T>>
+    nodes: Vec<TreeNode<T>>,
+    pub max_depth: usize
 }
 
 impl <T: PartialEq>Tree<T> {
     // Create a new tree with a single root node
     pub fn new(data: T) -> Tree<T> {
-        Tree { nodes: vec![TreeNode { data: data, index: 0, parent_index: None, children: Vec::new() }] }
+        Tree { nodes: vec![TreeNode { data: data, index: 0, parent_index: None, children: Vec::new(), depth: 0, is_root: true }], max_depth: 0 }
+    }
+
+    // Return the number of nodes in the tree
+    pub fn len(&self) -> usize {
+        self.nodes.len()
     }
 
     // Find all node indices that contain the specified value
@@ -53,8 +63,13 @@ impl <T: PartialEq>Tree<T> {
         let parent = &mut self.nodes[parent_index];
         parent.children.push(new_node_index);
 
+        let new_node_depth = parent.depth + 1;
+        if new_node_depth > self.max_depth {
+            self.max_depth = new_node_depth;
+        }
+
         // Create and save the new node
-        let new_node = TreeNode { data: data, index: new_node_index, parent_index: Some(parent_index), children: Vec::new() };
+        let new_node = TreeNode { data: data, index: new_node_index, parent_index: Some(parent_index), children: Vec::new(), depth: new_node_depth, is_root: false };
         self.nodes.push(new_node);
         new_node_index
     }
@@ -62,5 +77,31 @@ impl <T: PartialEq>Tree<T> {
     // Retrieve a node by index
     pub fn get_by_index(&self, index: usize) -> &TreeNode<T> {
         &self.nodes[index]
+    }
+    
+    // Find the index of the root node of this tree
+    // Defaults to the first node in the tree if no node has been marked as root
+    pub fn find_root_index(&self) -> usize {
+        let mut frontier = VecDeque::new();
+        frontier.push_back(0);
+
+        while !frontier.is_empty() {
+            // Breadth-first traversal
+            let index = frontier.pop_front().unwrap();
+            let node = &self.nodes[index];
+
+            // Add children to be searched next
+            for child in &node.children {
+                frontier.push_back(*child);
+            }
+
+            // Found the root node
+            if node.is_root {
+                return node.index;
+            }
+        }
+
+        // Assume the first node is the root node of the tree
+        0
     }
 }
